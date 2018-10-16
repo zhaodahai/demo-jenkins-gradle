5c94eac49e4fe48b27af11a41e952e3c8a63346d
dahai <>
2018-10-16 18:43:47 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoJenkinsGradleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJenkinsGradleApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController {
+    @GetMapping("/index")
+    public Map<String , String> index(){
+        Map<String, String> result = new HashMap<>();
+        result.put("test1", "test1");
+        result.put("test2", "test2");
+        result.put("test3", "test3");
+        return result;
+    }
+
+}
